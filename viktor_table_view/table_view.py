7fc9e337f86d7b99c01f78780a2c2e446af58742
rvdijk <rvandijk@viktor.ai>
2023-10-12 10:47:02 +0200

    Update header style only if no header style is given

--- viktor_table_view/table_view.py
+++ viktor_table_view/table_view.py
@@ -42,6 +42,7 @@ class TableResult(WebResult):
     ):
         self.dataframe = dataframe.copy(deep=True)
         self.style = style or self.dataframe.style
+        self.update_header_style()
         self.dataframe_colours = dataframe_colours
         self.n_decimals = n_decimals
         super().__init__(html=self.get_html())
@@ -77,3 +78,18 @@ class TableResult(WebResult):
                 template, {"table_html": self.style.to_html(table_attributes='class="table table-hover"')}
             )
         return result
+
+    def update_header_style(self):
+        """Updates header style if no header style is added to the styler"""
+        has_header_style = False
+        if self.style.table_styles:
+            for table_style in self.style.table_styles:
+                if "th" in table_style.values():
+                    has_header_style = True
+                    break
+        if not has_header_style or self.style.table_styles is None:
+            header_style = {
+                "selector": "th",
+                "props": [("text-align", "center"), ("background-color", "rgba(245, 245, 252)")],
+            }
+            self.style.set_table_styles([header_style])
